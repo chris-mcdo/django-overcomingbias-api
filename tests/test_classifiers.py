59e313107fa0d283d27595004348aa8691b0d4d2
chris-mcdo <cmcdonal33@gmail.com>
2022-04-11 16:14:48 +0100

    add alias uniqueness validation tests

--- tests/test_classifiers.py
+++ tests/test_classifiers.py
@@ -1,6 +1,8 @@
 import pytest
+from django.core.exceptions import ValidationError
 from django.db import IntegrityError
 from obapi.models import Idea, Topic
+from obapi.models.classifiers import IdeaAlias, TopicAlias
 
 
 @pytest.mark.django_db
@@ -89,3 +91,42 @@ class TestCreateAliasedModel:
         # Slug = existing alias
         with pytest.raises(IntegrityError):
             Topic.objects.create(name="Law, Etc.")
+
+
+@pytest.mark.django_db
+class TestValidateUniqueAlias:
+    def test_succeeds_for_unique_aliases(self):
+        # Arrange
+        law = Topic.objects.create(name="Law", description="About law.")
+        law.aliases.create(text="Legal")
+        norms = Topic.objects.create(name="Norms", description="About norms.")
+        social = TopicAlias(text="Social Norms", owner=norms)
+
+        # Act & assert
+        assert social.validate_unique() is None  # not yet saved
+        social.save()
+        assert social.validate_unique() is None
+
+    def test_succeeds_when_another_object_type_has_same_alias(self):
+        # Arrange
+        law_topic = Topic.objects.create(name="Law", description="About law.")
+        law_topic.aliases.create(text="Rules")
+        norms_idea = Idea.objects.create(name="Norms", description="About norms.")
+        rules = IdeaAlias(text="Rules", owner=norms_idea)
+
+        # Act & assert
+        assert rules.validate_unique() is None  # not yet saved
+        rules.save()
+        assert rules.validate_unique() is None
+
+    @pytest.mark.parametrize("alias_text", ["Law", "law", "Rules", "rulEs"])
+    def test_fails_with_duplicate_aliases(self, alias_text):
+        # Arrange
+        law = Topic.objects.create(name="Law", description="About law.")
+        law.aliases.create(text="Rules")
+        norms = Topic.objects.create(name="Norms", description="About norms.")
+
+        # Assert
+        norms_alias = TopicAlias(text=alias_text, owner=norms)
+        with pytest.raises(ValidationError):
+            norms_alias.validate_unique()
