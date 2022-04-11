164625b3c9bfe839c13ac654f9022421d5aea7af
chris-mcdo <cmcdonal33@gmail.com>
2022-04-11 12:44:06 +0100

    basic test config

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -1,5 +1,30 @@
-SECRET_KEY = 'fake-key'
+# Applications
+
 INSTALLED_APPS = [
-    "tests",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "obapi",
 ]
-DEBUG = True
+
+# Secrets
+SECRET_KEY = "fake-key"
+
+# Debug mode
+DEBUG = False
+
+# Internationalization
+# https://docs.djangoproject.com/en/4.0/topics/i18n/
+
+
+USE_TZ = True
+
+TIME_ZONE = "UTC"
+
+# Database
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": ":memory:",
+    }
+}
