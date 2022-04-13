9836196f0409c8cfc12452b72edd240f6df7a0b8
chris-mcdo <cmcdonal33@gmail.com>
2022-04-13 17:54:41 +0100

    add app verbose name

--- obapi/apps.py
+++ obapi/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class OBAPIConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "obapi"
+    verbose_name = "Overcoming Bias API"
