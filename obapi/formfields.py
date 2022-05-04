3959227b636c0ff68d854f8dd5c50c10e2f1221d
chris-mcdo <cmcdonal33@gmail.com>
2022-05-04 14:54:57 +0100

    add export format form field

--- obapi/formfields.py
+++ obapi/formfields.py
@@ -1,7 +1,34 @@
 from django import forms
 
-from obapi import validators
+import obapi.export
+import obapi.validators
 
 
 class SimpleSlugField(forms.CharField):
-    default_validators = [validators.validate_simple_slug]
+    default_validators = [obapi.validators.validate_simple_slug]
+
+
+class PandocWriterField(forms.ChoiceField):
+    """A form field for selecting a Sequence export format."""
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.writers = {
+            Writer.pandoc_name: Writer
+            for Writer in obapi.export.get_supported_writers()
+        }
+
+        self.choices = [
+            (writer_name, writer_name) for writer_name in self.writers.keys()
+        ]
+
+    def clean(self, value):
+        value = super().clean(value)
+        try:
+            return self.writers[value]
+        except KeyError:
+            raise forms.ValidationError(
+                self.error_messages["invalid_choice"],
+                code="invalid_choice",
+                params={"value": value},
+            )
